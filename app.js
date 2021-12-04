d64283212d952d46d608b7f3d61aeaeee598f5b4
Stephani Silvia Zatta <stephani.zatta@ambevtech.com.br>
2021-12-03 21:05:44 -0300

    removido parametro que não estava sendo usado

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function(req, res){
+app.get("/", function(res){
     res.sendFile(__dirname + "/src/index.html");
 });
 
