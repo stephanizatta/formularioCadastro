73d8be5dd16757d7fd775e9406ad3af877298a52
Stephani Silvia Zatta <stephani.zatta@ambevtech.com.br>
2021-12-03 22:16:33 -0300

    feita aconexão com o banco de dados

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require("express");
-
 const app = express();
+
+//Conecta com o BD
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'prog',
+    password: '',
+    database: 'progweb',
+});
+
+connection.connect(function (err){
+    if(err){
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + connection.stack);
+});
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/src/index.html");
